12c40cee29bed03c3c5dcaa167dfcb1c57c39542
Peaboard <https://github.com/peaboard>
2017-05-06 22:01:53 +0530

    Added basic checks

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import os
 from app import app
 
 if os.geteuid() != 0:
-    print('\033[93m' + 'You need to have root privileges to run this script\n' + '\033[0m')
-    print('\033[94m' + "Please try again, this time using 'sudo'. Exiting.\n"  + '\033[0m')
-    exit
+    print('\033[93m' + '\nYou need to have root privileges to run this script\n' + '\033[0m')
+    print('\033[94m' + "Please try again, this time using 'sudo'.\nExiting.\n"  + '\033[0m')
+    exit()
 
 def main():
     app.run(debug=True, host='0.0.0.0', port=80)
