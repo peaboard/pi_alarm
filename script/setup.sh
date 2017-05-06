b65c9ca34e8e97767a87dfb33b54833552f60ebf
Peaboard <https://github.com/peaboard>
2017-05-06 22:02:44 +0530

    Added checks in setup and new packages to be added

--- script/setup.sh
+++ script/setup.sh
@@ -11,6 +11,16 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+if [[ "$VIRTUAL_ENV" = "" ]]; then
+   echo "It is suggested that you run setup script through virtualenv."
+   read -p "Do you want to continue? Y/n  " choice
+   case "$choice" in
+     y|Y ) echo "Continuing..";;
+     n|N ) exit 1;;
+     * ) echo "invalid";;
+   esac
+fi
+
 python_libs=(
     'flask==0.10.1'             # The web framework we're using
     'Mako==0.9.0'               # Mako templates for the site
@@ -19,9 +29,21 @@ python_libs=(
     'Flask-BasicAuth'           # for locking down the site
     'python-crontab==2.1.1'     # for accessing the crontab
     'RPi.GPIO'                  # for driving the GPIO pins on the Pi
+    'radiopy'                   # for Web Radio
 )
 
 # Install each of the libraries
 for i in "${python_libs[@]}"; do
     pip install $i
+done
+
+linux_packages=(
+    'mpd'
+    'mpc'
+)
+
+apt-get update
+
+for i in "${linux_packages[@]}"; do
+    apt-get install -y $i
 done
